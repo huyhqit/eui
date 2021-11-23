6f90d7afc91a2acb713976239e14967301e8f231
huyho <huy.ho@ekoios.vn>
2021-11-23 08:09:46 +0700

    Initial commit

---
+++ packages/components/lib/atoms/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+var Button = function (_a) {
+    var label = _a.label;
+    return React.createElement("button", null, label);
+};
+
+export { Button as default };
+//# sourceMappingURL=Button.js.map
---
+++ packages/demo/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Button } from '@eui/components';
+
+ReactDOM.render(<Button label="React" />, document.querySelector('#root'));
